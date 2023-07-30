0870ad59eb361cebda6742db77cc5b39e725ea27
HuynhTonyy <leongirl2013@gmail.com>
2023-07-30 13:28:11 +0700

    SaveLoadBtnDisplay

--- SoulValleyGame/Assets/Scripts/SaveLoad/SaveGameManager.cs
+++ SoulValleyGame/Assets/Scripts/SaveLoad/SaveGameManager.cs
@@ -3,17 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Photon.Pun
-
+using Photon.Pun;
+using UnityEngine.UI;
+using TMPro;
 public class SaveGameManager : MonoBehaviour
 {
     public static SaveData data;
 
-    PhotonView
+    [SerializeField] Button btnSave, btnLoad;
+    PhotonView view;
     private void Awake()
     {
+        view = GetComponent<PhotonView>();
         data = new SaveData();
         SaveLoad.OnLoadGame += LoadData;
+        if(!PhotonNetwork.IsMasterClient){
+            btnSave.gameObject.SetActive(false);
+            btnLoad.gameObject.SetActive(false);
+        }
     }
 
     public void DeleteData()
